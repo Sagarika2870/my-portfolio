770aab0e95c62ad182a87c5527bfbe8a24deee2d
SagarikaR <86086592+Sagarika2870@users.noreply.github.com>
2022-01-04 18:39:06 -0500

    Revert "fixed index.js"
    This reverts commit ab84eb767d4c913e1bb9f6d8d3b8759c3d0345c5.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import {Router, hashHistory as history} from react-router;
+import routes from './Routes';
 
 ReactDOM.render(
   <BrowserRouter>
