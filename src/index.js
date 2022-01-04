7b2604497c38f7a82f9bdce716d9d62e21b1fb7b
SagarikaR <86086592+Sagarika2870@users.noreply.github.com>
2022-01-04 18:38:05 -0500

    Revert "typo in index.js"
    This reverts commit 48bb85ccb1685b386e18bd9763039e2c05059583.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowswerRouter} from 'react-router-dom';
 import routes from './Routes';
 
 
 ReactDOM.render((
-  <BrowserRouter>
+  <BrowswerRouter>
     <App />
-  </BrowserRouter>),
+  </BrowswerRouter>),
   document.getElementById("about")
 
 );
