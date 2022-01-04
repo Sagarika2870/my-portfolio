909f64093297b96cf78d657af39673cd94a81f6c
SagarikaR <86086592+Sagarika2870@users.noreply.github.com>
2022-01-04 17:10:33 -0500

    useHistory fix

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {Router, useHistory as history} from 'react-router-dom';
+import {Router, useHistory} from 'react-router-dom';
 import routes from './Routes';
 
 
 ReactDOM.render(
-  <Router routes={Routes} history={history} />,
+  <Router routes={Routes} useHistory={useHistory} />,
   document.getElementById("about")
 
 );
