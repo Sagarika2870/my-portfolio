7618761788c10e76444dc9ed23d508811347ddb9
SagarikaR <86086592+Sagarika2870@users.noreply.github.com>
2022-01-04 18:37:59 -0500

    Revert "trying to add pages"
    This reverts commit fba8e8328f81e2faa1dc7b85e0ca792cdb7dc6a4.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
-
+import routes from './Routes';
 
 
 ReactDOM.render((
   <BrowserRouter>
     <App />
   </BrowserRouter>),
-  document.getElementById("root")
+  document.getElementById("about")
 
 );
