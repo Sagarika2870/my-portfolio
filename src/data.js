172dff202cbd39889c0897c34b3a337db3d2b000
SagarikaR <86086592+Sagarika2870@users.noreply.github.com>
2022-01-04 12:01:56 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: "Seam Carving",
+    subtitle: "C",
+    description:
+      "Image resizing technique that enlarges or shrinks images pixel-by-pixel",
+    image: "./Seam-carving.gif",
+    link: "https://github.com/Sagarika2870/Seam-Carving",
+  },
+  {
+    title: "Meet the Bears (In progress)",
+    subtitle: "Unity and C#",
+    description:
+      "Virtual and interactive polar bear habitat based on the Canadian Polar Bear Habitat",
+    image: "./polar-bear.jpg",
+    link: "https://github.com/Sagarika2870/Meet-The-Bears",
+  },
+  {
+    title: "Big Data Challenge",
+    subtitle: "Python and Azure",
+    description:
+      "Analyzes the correlation between political ideologies on promoting/interactingwith misinformation on Twitter",
+    image: "./twitter.png",
+    link: "https://github.com/Sagarika2870/Big-Data-Challenge",
+  },
+  {
+    title: "Finding Synonyms",
+    subtitle: "Python",
+    description:
+      "Finding synonyms of words using the symantic similarity",
+    image: "./synonyms.jpg",
+    link: "https://github.com/Sagarika2870/Finding-Synonyms",
+  },
+];
+
+
+
+export const skills = [
+  "Python",
+  "C",
+  "C++",
+  "Javascript",
+  "React",
+  "HTML",
+  "CSS",
+];
