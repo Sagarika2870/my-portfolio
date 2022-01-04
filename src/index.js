c9448245504011456cfb7de07d5d9a4f5e9d2a3a
SagarikaR <86086592+Sagarika2870@users.noreply.github.com>
2022-01-04 18:38:11 -0500

    Revert "changed to browserrouter"
    This reverts commit 0c42482375fff0268980a8d21acaa7e0eeb055c9.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowswerRouter} from 'react-router-dom';
+import {Router, useHistory} from 'react-router-dom';
 import routes from './Routes';
 
 
-ReactDOM.render((
-  <BrowswerRouter>
-    <App />
-  </BrowswerRouter>),
+ReactDOM.render(
+  <Router routes={Routes} useHistory={useHistory} />,
   document.getElementById("about")
 
 );
