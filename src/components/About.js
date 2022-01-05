908f408a39ff0efaf7cc17eb2d88fd799245f13d
SagarikaR <86086592+Sagarika2870@users.noreply.github.com>
2022-01-04 20:16:53 -0500

    lighter pink

--- src/components/About.js
+++ src/components/About.js
@@ -15,14 +15,14 @@ export default function About(){
                         
                         <a
                             href="#contact"
-                            className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                            className="inline-flex text-white bg-cyan-500 border-0 py-2 px-6 focus:outline-none hover:bg-cyan-600 rounded text-lg">
                             Contact Me
                         </a>
                         <a
 
                             href="./Sagarika_Resume.pdf"
 
-                            className="ml-4 inline-flex text-gray-400 bg-gray-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-800 hover:text-white rounded text-lg">
+                            className="ml-4 inline-flex text-gray-400 bg-fuchsia-500 border-0 py-2 px-6 focus:outline-none hover:bg-fuchsia-800 hover:text-white rounded text-lg">
                             Resume
                         </a>
    
