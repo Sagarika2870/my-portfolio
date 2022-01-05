efd96a627e755211c0a131d03cb9d505ca733db1
SagarikaR <86086592+Sagarika2870@users.noreply.github.com>
2022-01-04 21:03:24 -0500

    colour

--- src/components/About.js
+++ src/components/About.js
@@ -22,7 +22,7 @@ export default function About(){
 
                             href="./Sagarika_Resume.pdf"
 
-                            className="ml-4 inline-flex text-gray-400 bg-sky-500 border-0 py-2 px-6 focus:outline-none hover:bg-sky-800 hover:text-white rounded text-lg">
+                            className="ml-4 inline-flex text-white bg-sky-500 border-0 py-2 px-6 focus:outline-none hover:bg-sky-800 hover:text-white rounded text-lg">
                             Resume
                         </a>
    
